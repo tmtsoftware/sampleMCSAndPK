07047ecea22982c1d960788a5a542bab1abbb617
Pritam Kadam <kpritam@thoughtworks.com>
2019-01-24 22:57:19 +0530

    Use event generator API of event service instead of custom scheduler.

--- samplepk/samplepkAssembly/src/main/java/com/tmt/tcs/pk/samplepkAssembly/EventHandlerActor.java
+++ samplepk/samplepkAssembly/src/main/java/com/tmt/tcs/pk/samplepkAssembly/EventHandlerActor.java
@@ -1,9 +1,9 @@
 package com.tmt.tcs.pk.samplepkAssembly;
 
+import akka.actor.Cancellable;
 import akka.actor.typed.Behavior;
 import akka.actor.typed.javadsl.*;
 import csw.event.api.javadsl.IEventService;
-import csw.logging.javadsl.ILogger;
 import csw.logging.javadsl.JLoggerFactory;
 import csw.params.core.generics.Key;
 import csw.params.core.models.Prefix;
@@ -11,16 +11,16 @@ import csw.params.events.Event;
 import csw.params.events.EventName;
 import csw.params.events.SystemEvent;
 import csw.params.javadsl.JKeyType;
+
+import java.time.Duration;
 import java.time.Instant;
-import java.util.concurrent.*;
+import java.util.function.Supplier;
+
 public class EventHandlerActor extends AbstractBehavior<EventHandlerActor.EventMessage> {
-    private ActorContext<EventMessage> actorContext;
     private IEventService eventService;
-    private JLoggerFactory loggerFactory;
-    private ILogger log;
 
     private static final Prefix prefix = new Prefix("tcs.pk");
-
+    private Cancellable cancellable;
     private int counterMcs = 0;
 
     private static final int LIMIT = 100000;
@@ -28,36 +28,32 @@ public class EventHandlerActor extends AbstractBehavior<EventHandlerActor.EventM
     private Key<Double> elDoubleKey = JKeyType.DoubleKey().make("mcs.el");
     private Key<Instant> publishTimeKey = JKeyType.TimestampKey().make("timeStamp");
 
-    private static final ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
-
     private EventHandlerActor(ActorContext<EventMessage> actorContext, IEventService eventService, JLoggerFactory loggerFactory) {
-        this.actorContext = actorContext;
         this.eventService = eventService;
-        this.loggerFactory = loggerFactory;
-        this.log = loggerFactory.getLogger(actorContext, getClass());
 
     }
 
-    public static <EventMessage> Behavior<EventMessage> behavior(IEventService eventService, JLoggerFactory loggerFactory) {
-        return Behaviors.setup(ctx -> {
-            return (AbstractBehavior<EventMessage>) new EventHandlerActor((ActorContext<EventHandlerActor.EventMessage>) ctx, eventService, loggerFactory);
-        });
+    static <EventMessage> Behavior<EventMessage> behavior(IEventService eventService, JLoggerFactory loggerFactory) {
+        return Behaviors.setup(ctx -> (AbstractBehavior<EventMessage>) new EventHandlerActor((ActorContext<EventHandlerActor.EventMessage>) ctx, eventService, loggerFactory));
     }
-    private Runnable demandGenerator = new Runnable() {
+
+    private Supplier<Event> demandGenerator = new Supplier<Event>() {
         double az = 0.0000001;
         double el = 0.0000001;
+
         @Override
-        public void run() {
+        public Event get() {
             counterMcs++;
-            if(counterMcs < 100000){
-                az += 0.0000001;
-                el += 0.0000001;
-                Event event = new SystemEvent(prefix, new EventName("mcsdemandpositions"))
+            az += 0.0000001;
+            el += 0.0000001;
+            if (counterMcs < LIMIT) {
+                return new SystemEvent(prefix, new EventName("mcsdemandpositions"))
                         .add(azDoubleKey.set(az))
                         .add(elDoubleKey.set(el))
                         .add(publishTimeKey.set(Instant.now()));
-                eventService.defaultPublisher().publish(event);
             }
+            cancellable.cancel();
+            return null;
         }
     };
 
@@ -67,13 +63,12 @@ public class EventHandlerActor extends AbstractBehavior<EventHandlerActor.EventM
         ReceiveBuilder<EventMessage> builder = receiveBuilder()
                 .onMessage(McsDemandMessage.class,
                         message -> {
-                            scheduledExecutorService.scheduleWithFixedDelay(demandGenerator,10,10,TimeUnit.MILLISECONDS);
+                            cancellable = eventService.defaultPublisher().publish(demandGenerator, Duration.ofMillis(10));
                             return Behaviors.same();
                         });
 
         return builder.build();
     }
-
 
 
     // add messages here
@@ -84,7 +79,7 @@ public class EventHandlerActor extends AbstractBehavior<EventHandlerActor.EventM
         private final double az;
         private final double el;
 
-        public McsDemandMessage(double az, double el){
+        public McsDemandMessage(double az, double el) {
             this.az = az;
             this.el = el;
         }
